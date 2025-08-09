5d0a1be523cad121c02ab43b07e47b9d842c9dfe
Jacob Chaffin <jchaffin@ucla.edu>
2025-08-08 23:11:57 -0700

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,19 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {},
+  allConfig: {},
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
 ];
 
 export default eslintConfig;
